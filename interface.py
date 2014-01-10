4cce683cb72f2d37e34339db0ed45cef39f8ca0e
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-01-11 03:10:52 +0400

    Added course addition

--- interface.py
+++ interface.py
@@ -7,8 +7,9 @@ import sqlite3
 
 cgitb.enable()
 
-print "Content-type: text/javascript"
-print ""
+def print_header():
+	print "Content-type: text/javascript"
+	print ""
 
 form = cgi.FieldStorage()
 if "query" not in form:
@@ -20,6 +21,7 @@ else:
 		cursor.execute("select uuid, name from materials")
 		js=json.dumps({"error": 0, "materials": cursor.fetchall()})
 		conn.close()
+		print_header()
 		print js
 	if form["query"].value == "courses":
 		conn = sqlite3.connect("materials.sqlite")
@@ -27,16 +29,34 @@ else:
 		cursor.execute("select uuid, name from courses")
 		js=json.dumps({"error": 0, "courses": cursor.fetchall()})
 		conn.close()
+		print_header()
 		print js
 	if form["query"].value == "add_material":
+		print_header()
 		print "Здесь должна быть заливка и проверка материалов"
 	if form["query"].value == "delete_material":
+		print_header()
 		print "Здесь должно быть удаление материалов"
 	if form["query"].value == "delete_course":
+		print_header()
 		print "Здесь должно быть удаление курсов"
 	if form["query"].value == "add_course":
-		print "Здесь должно быть добавление курсов"
+		if "name" in form:
+			conn = sqlite3.connect("materials.sqlite")
+			cursor = conn.cursor()
+			t = form["name"].value
+			cursor.execute("insert into courses (uuid, name) select *, ? from next_uuid", (t,))
+			conn.commit()
+			js=json.dumps({"error": 0, "courses": cursor.fetchall()})
+			conn.close()
+			print_header()
+			print js
+		else:
+			print_header()
+			print json.dumps({"error": 1 })
 	if form["query"].value == "add_belongs":
+		print_header()
 		print "Здесь должна быть обработка принадлежности"
 	if form["query"].value == "delete_belongs":
+		print_header()
 		print "Здесь должна быть обработка удаления принадлежности"
