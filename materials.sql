db85f4de0fb148737c70ab82903313b5e07133e1
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-01-11 16:20:07 +0400

    Added description fields to courses and materials

--- materials.sql
+++ materials.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-CREATE TABLE materials (id integer primary key asc autoincrement, uuid text unique not null, name text not null);
-CREATE TABLE courses (id integer primary key asc autoincrement, uuid text unique not null, name text not null);
+CREATE TABLE materials (id integer primary key asc autoincrement, uuid text unique not null, name text unique not null, description text);
+CREATE TABLE courses (id integer primary key asc autoincrement, uuid text unique not null, name text unique not null, description text);
 CREATE TABLE belongs (id integer primary key asc autoincrement, material_uuid text references materials (uuid) on update restrict on delete restrict, year integer, course_uuid text references courses (uuid) on update restrict on delete restrict);
 DELETE FROM sqlite_sequence;
 INSERT INTO "sqlite_sequence" VALUES('materials',1);
