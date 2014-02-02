664e1ece3a1153bf6bf2c885c2509bdb41f16e48
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-02-02 19:21:13 +0400

    Refactored opening database sequence

--- interface.py
+++ interface.py
@@ -11,13 +11,18 @@ def print_header():
 	print "Content-type: text/javascript; charset=utf-8"
 	print ""
 
+def db_open():
+	conn = sqlite3.connect("materials.sqlite")
+	conn.execute('pragma foreign_keys = on')
+	return conn
+
+
 form = cgi.FieldStorage()
 if "query" not in form:
 	print json.dumps({"error": 1 });
 else:
 	if form["query"].value == "materials":
-		conn = sqlite3.connect("materials.sqlite")
-		conn.execute('pragma foreign_keys = on')
+		conn = db_open()
 		cursor = conn.cursor()
 		cursor.execute("select uuid, name,description,owner, upload_date, edit_date from materials")
 		js=json.dumps({"error": 0, "materials": cursor.fetchall()})
@@ -26,8 +31,7 @@ else:
 		print js
 	if form["query"].value == "author_for_material":
 		if "uuid" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t1 = form["uuid"].value
 			cursor.execute("select authorship.author_uuid, authors.fio from authorship,authors where authorship.material_uuid = ? and authors.uuid = authorship.author_uuid", (t1,))
@@ -40,8 +44,7 @@ else:
 			print_header()
 			print json.dumps({"error": 1 })
 	if form["query"].value == "discipline":
-		conn = sqlite3.connect("materials.sqlite")
-		conn.execute('pragma foreign_keys = on')
+		conn = db_open()
 		cursor = conn.cursor()
 		cursor.execute("select uuid, name,semester,description from discipline")
 		js=json.dumps({"error": 0, "discipline": cursor.fetchall()})
@@ -49,8 +52,7 @@ else:
 		print_header()
 		print js
 	if form["query"].value == "authors":
-		conn = sqlite3.connect("materials.sqlite")
-		conn.execute('pragma foreign_keys = on')
+		conn = db_open()
 		cursor = conn.cursor()
 		cursor.execute("select uuid, fio from authors")
 		js=json.dumps({"error": 0, "authors": cursor.fetchall()})
@@ -58,8 +60,7 @@ else:
 		print_header()
 		print js
 	if form["query"].value == "speciality":
-		conn = sqlite3.connect("materials.sqlite")
-		conn.execute('pragma foreign_keys = on')
+		conn = db_open()
 		cursor = conn.cursor()
 		cursor.execute("select uuid, code, name, description from speciality")
 		js=json.dumps({"error": 0, "speciality": cursor.fetchall()})
@@ -67,8 +68,7 @@ else:
 		print_header()
 		print js
 	if form["query"].value == "study_form":
-		conn = sqlite3.connect("materials.sqlite")
-		conn.execute('pragma foreign_keys = on')
+		conn = db_open()
 		cursor = conn.cursor()
 		cursor.execute("select uuid, study_form from study_form")
 		js=json.dumps({"error": 0, "study_form": cursor.fetchall()})
@@ -83,8 +83,7 @@ else:
 		print "Здесь должно быть удаление материалов"
 	if form["query"].value == "add_discipline":
 		if "name" in form and "sem" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t1 = form["name"].value
 			t2 = form["sem"].value
@@ -103,8 +102,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "delete_discipline":
 		if "uuid" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["uuid"].value
 			cursor.execute("delete from discipline where uuid = ?", (t,))
@@ -118,8 +116,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "add_author":
 		if "fio" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["fio"].value
 			cursor.execute("insert into authors (uuid, fio) select *, ? from next_uuid", (str(t).decode('utf-8'),))
@@ -133,8 +130,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "delete_author":
 		if "uuid" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["uuid"].value
 			cursor.execute("delete from authors where uuid = ?", (t,))
@@ -148,8 +144,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "add_speciality":
 		if "name" in form and "code" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t1 = form["name"].value
 			t2 = form["code"].value
@@ -168,8 +163,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "delete_speciality":
 		if "uuid" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["uuid"].value
 			cursor.execute("delete from speciality where uuid = ?", (t,))
@@ -183,8 +177,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "add_study_form":
 		if "name" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["name"].value
 			cursor.execute("insert into study_form (uuid, study_form) select *, ? from next_uuid", (str(t).decode('utf-8'),))
@@ -198,8 +191,7 @@ else:
 			print json.dumps({"error": 1 })
 	if form["query"].value == "delete_study_form":
 		if "uuid" in form:
-			conn = sqlite3.connect("materials.sqlite")
-			conn.execute('pragma foreign_keys = on')
+			conn = db_open()
 			cursor = conn.cursor()
 			t = form["uuid"].value
 			cursor.execute("delete from study_form where uuid = ?", (t,))
