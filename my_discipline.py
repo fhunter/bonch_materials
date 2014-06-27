e804a3bd767dbc6849f22b1e7dbf87debd5002f4
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-06-27 17:09:03 +0400

    Refactored UI code

--- my_discipline.py
+++ my_discipline.py
@@ -9,6 +9,7 @@ discipline_page=header_include + menu_include + u"""
 	  <h2>Управление списком дисциплин</h2>
 	  <div class="add_form">
 	  <form id="discipline_add_form" method="post" action="">
+	    <input type="hidden" name="action" value="add"/>
 	    Название:<input name="discipline_name"><br>
 	    Семестр:<input name="discipline_semester"><br>
 	    Описание:<br>
@@ -32,23 +33,29 @@ def get_discipline():
 	result = db_exec_sql("select uuid, name,semester,description from discipline")
 	return result
 
-def add_discipline(name,semester,description):
-	db_exec_sql("insert into discipline (uuid, name, semester, description) select *, ?, ?, ? from next_uuid", (str(name).decode('utf-8'),str(semester).decode('utf-8'),str(description).decode('utf-8'),))
+def add_discipline(form):
+	if ("discipline_name" in form) and ("discipline_semester" in form):
+		name = cgi.escape(form.getfirst("discipline_name",""))
+		semester = cgi.escape(form.getfirst("discipline_semester",""))
+		description = cgi.escape(form.getfirst("discipline_description",""))
+		db_exec_sql("insert into discipline (uuid, name, semester, description) select *, ?, ?, ? from next_uuid", (str(name).decode('utf-8'),str(semester).decode('utf-8'),str(description).decode('utf-8'),))
 
-def del_discipline(uuid):
-	db_exec_sql("delete from discipline where uuid = ?", (uuid,))
+def del_discipline(form):
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		db_exec_sql("delete from discipline where uuid = ?", (uuid,))
+
+def edit_discipline(form):
+	pass
+
+discipline_case = { "edit": edit_discipline, "delete": del_discipline, "add": add_discipline }
 
 def discipline_showui(form):
 	header_html()
 	if is_post():
-		if ("discipline_name" in form) and ("discipline_semester" in form):
-			discipline_name = cgi.escape(form.getfirst("discipline_name",""))
-			discipline_semester = cgi.escape(form.getfirst("discipline_semester",""))
-			discipline_description = cgi.escape(form.getfirst("discipline_description",""))
-			add_discipline(discipline_name,discipline_semester,discipline_description)
-		if "uuid" in form:
-			uuid = cgi.escape(form.getfirst("uuid",""))
-			del_discipline(uuid)
+		action = form.getfirst("action","")
+		if action in discipline_case:
+			discipline_case[action](form)
 	result=get_discipline()
 	table = gen_table(result, (u"Название",u"Семестр",u"Описание"),(False,False,True))
 	page = discipline_page % (table, )
