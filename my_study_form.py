818ff673d7353fc7d6c414b630372f0f867eb260
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-03 01:04:55 +0400

    Added study form editing

--- my_study_form.py
+++ my_study_form.py
@@ -26,6 +26,23 @@ study_form_page=header_include + menu_include + u"""
     </div>
 """
 
+study_form_edit = header_include + menu_include + u"""
+      <div id="UI_elements">
+	<div id="material_admin" class="UI_tab" >
+	  <h2>Редактирование формы обучения</h2>
+	  <div class="add_form">
+	  <form id="study_form_edit_form" method="post" action="">
+	    <input type="hidden" name="action" value="update"/>
+	    <input type="hidden" name="uuid" value="%s"/>
+	    Название:<input name="study_form_name" value="%s"><br>
+	    <input type=submit value="Отредактировать">
+	  </form>
+	  </div>
+	</div>
+	</div>
+	</div>
+	"""
+
 def get_study_form():
 	result = db_exec_sql("select uuid, study_form from study_form")
 	return result
@@ -41,7 +58,22 @@ def del_study_form(form):
 		db_exec_sql("delete from study_form where uuid = ?", (uuid,))
 
 def edit_study_form(form):
-	pass
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		material = db_exec_sql("select uuid, study_form from study_form where uuid = ?", (uuid,))
+		material= material[0]
+		page = study_form_edit % (material[0],material[1],)
+		print_ui(page )
+		exit(0)
+
+def update_study_form(form):
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		study_form = db_exec_sql("select uuid, study_form from study_form where uuid = ?", (uuid,))[0]
+		name = study_form[1]
+		if "study_form_name" in form:
+			name= cgi.escape(form.getfirst("study_form_name",""))
+			db_exec_sql("update study_form set study_form= ? where uuid = ?", (name.decode('utf-8'), uuid,))
 
 def study_form_mainpage():
 	result=get_study_form()
@@ -49,7 +81,7 @@ def study_form_mainpage():
 	page = study_form_page % (table, )
 	print_ui(page)
 
-study_form_case = { "edit": edit_study_form, "delete": del_study_form, "add": add_study_form }
+study_form_case = { "edit": edit_study_form, "delete": del_study_form, "add": add_study_form, "update": update_study_form, }
 
 def study_form_showui(form):
 	if is_post():
