6cc0c854aa4d8c307a78057ca651d913a717aa24
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-03 01:12:33 +0400

    Added author editing

--- my_author.py
+++ my_author.py
@@ -25,6 +25,23 @@ authors_page=header_include + menu_include + u"""
 	</div>
 	"""
 
+authors_edit = header_include + menu_include + u"""
+      <div id="UI_elements">
+	<div id="material_admin" class="UI_tab" >
+	  <h2>Редактирование автора</h2>
+	  <div class="add_form">
+	  <form id="study_form_edit_form" method="post" action="">
+	    <input type="hidden" name="action" value="update"/>
+	    <input type="hidden" name="uuid" value="%s"/>
+	    ФИО:<input name="authors_name" value="%s"><br>
+	    <input type=submit value="Отредактировать">
+	  </form>
+	  </div>
+	</div>
+	</div>
+	</div>
+	"""
+
 def get_authors():
 	result = db_exec_sql("select uuid, fio from authors")
 	return result
@@ -40,9 +57,24 @@ def del_authors(form):
 		db_exec_sql("delete from authors where uuid = ?", (uuid,))
 
 def edit_authors(form):
-	pass
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		author = db_exec_sql("select uuid, fio from authors where uuid = ?", (uuid,))
+		author= author[0]
+		page = authors_edit % (author[0],author[1],)
+		print_ui(page )
+		exit(0)
 
-authors_case = { "edit": edit_authors, "delete": del_authors, "add": add_authors }
+def update_authors(form):
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		study_form = db_exec_sql("select uuid, fio from authors where uuid = ?", (uuid,))[0]
+		name = study_form[1]
+		if "authors_name" in form:
+			name= cgi.escape(form.getfirst("authors_name",""))
+			db_exec_sql("update authors set fio= ? where uuid = ?", (name.decode('utf-8'), uuid,))
+
+authors_case = { "edit": edit_authors, "delete": del_authors, "add": add_authors, "update": update_authors }
 
 def authors_showui(form):
 	if is_post():
