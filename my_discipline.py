a865988078e364e0e9d39a768fa013fbb24244d2
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-12 23:30:10 +0400

    Implemented editing

--- my_discipline.py
+++ my_discipline.py
@@ -28,6 +28,25 @@ discipline_page=header_include + menu_include + u"""
 	</div>
 	</div>
 	"""
+discipline_edit = header_include + menu_include + u"""
+      <div id="UI_elements">
+	<div id="material_admin" class="UI_tab" >
+	  <h2>Редактирование дисциплины</h2>
+	  <div class="add_form">
+	  <form id="study_form_edit_form" method="post" action="">
+	    <input type="hidden" name="action" value="update"/>
+	    <input type="hidden" name="uuid" value="%s"/>
+	    Название:<input name="discipline_name" value="%s"><br>
+	    Семестр:<input name="discipline_semester" value="%s"><br>
+	    Описание<br>
+	    <textarea name="discipline_description">%s</textarea><br>
+	    <input type=submit value="Отредактировать">
+	  </form>
+	  </div>
+	</div>
+	</div>
+	</div>
+	"""
 
 def get_discipline():
 	result = db_exec_sql("select uuid, name,semester,description from discipline")
@@ -50,9 +69,30 @@ def del_discipline(form):
 		db_exec_sql("delete from discipline where uuid = ?", (uuid,))
 
 def edit_discipline(form):
-	pass
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		discipline = db_exec_sql("select uuid, name, description,semester from discipline where uuid = ?", (uuid,))[0]
+		page = discipline_edit % ( discipline[0], discipline[1], discipline[3], discipline[2])
+		print_ui(page)
 
-discipline_case = { "edit": edit_discipline, "delete": del_discipline, "add": add_discipline }
+def update_discipline(form):
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		discipline = db_exec_sql("select uuid, name, semester, description from discipline where uuid = ?", (uuid,))[0]
+		name = discipline[1]
+		semester = discipline[2]
+		description = discipline[3]
+		if "discipline_name" in form:
+			name= cgi.escape(form.getfirst("discipline_name",""))
+			db_exec_sql("update discipline set name= ? where uuid = ?", (name.decode('utf-8'), uuid,))
+		if "discipline_semester" in form:
+			semester= cgi.escape(form.getfirst("discipline_semester",""))
+			db_exec_sql("update discipline set semester= ? where uuid = ?", (semester.decode('utf-8'), uuid,))
+		if "discipline_description" in form:
+			description = cgi.escape(form.getfirst("discipline_description",""))
+			db_exec_sql("update discipline set description= ? where uuid = ?", (description.decode('utf-8'), uuid,))
+
+discipline_case = { "edit": edit_discipline, "delete": del_discipline, "add": add_discipline, "update": update_discipline }
 
 def discipline_showui(form):
 	if is_post():
