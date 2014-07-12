426304da9c532677080164e27d42cfaab5f0d690
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-12 23:20:49 +0400

    Implemented editing speciality

--- my_speciality.py
+++ my_speciality.py
@@ -28,6 +28,26 @@ speciality_page=header_include + menu_include + u"""
 	</div>
 	"""
 
+speciality_edit = header_include + menu_include + u"""
+      <div id="UI_elements">
+	<div id="material_admin" class="UI_tab" >
+	  <h2>Редактирование автора</h2>
+	  <div class="add_form">
+	  <form id="study_form_edit_form" method="post" action="">
+	    <input type="hidden" name="action" value="update"/>
+	    <input type="hidden" name="uuid" value="%s"/>
+	    Шифр:<input name="speciality_code" value="%s"><br>
+	    Название:<input name="speciality_name" value="%s"><br>
+	    Описание<br>
+	    <textarea name="speciality_description">%s</textarea><br>
+	    <input type=submit value="Отредактировать">
+	  </form>
+	  </div>
+	</div>
+	</div>
+	</div>
+	"""
+
 
 def get_speciality():
 	result = db_exec_sql("select uuid, code, name, description from speciality")
@@ -52,8 +72,28 @@ def del_speciality(form):
 def edit_speciality(form):
 	if "uuid" in form:
 		uuid = cgi.escape(form.getfirst("uuid",""))
+		speciality = db_exec_sql("select uuid, name, code, description from speciality where uuid = ?", (uuid,))[0]
+		page = speciality_edit % ( speciality[0], speciality[1], speciality[2], speciality[3])
+		print_ui(page)
 
-speciality_case = { "edit": edit_speciality, "delete": del_speciality, "add": add_speciality }
+def update_speciality(form):
+	if "uuid" in form:
+		uuid = cgi.escape(form.getfirst("uuid",""))
+		speciality = db_exec_sql("select uuid, name, code, description from speciality where uuid = ?", (uuid,))[0]
+		name = speciality[1]
+		code = speciality[2]
+		description = speciality[3]
+		if "speciality_name" in form:
+			name= cgi.escape(form.getfirst("speciality_name",""))
+			db_exec_sql("update speciality set name= ? where uuid = ?", (name.decode('utf-8'), uuid,))
+		if "speciality_code" in form:
+			code= cgi.escape(form.getfirst("speciality_code",""))
+			db_exec_sql("update speciality set code= ? where uuid = ?", (code.decode('utf-8'), uuid,))
+		if "speciality_description" in form:
+			description = cgi.escape(form.getfirst("speciality_name",""))
+			db_exec_sql("update speciality set description= ? where uuid = ?", (description.decode('utf-8'), uuid,))
+
+speciality_case = { "edit": edit_speciality, "delete": del_speciality, "add": add_speciality, "update": update_speciality }
 
 def speciality_showui(form):
 	if is_post():
