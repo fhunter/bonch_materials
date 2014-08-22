0c43ad69d21854f0fb4681a96c6ffec34ee7b397
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-08-22 21:01:40 +0400

    Added script for cron

---
+++ script/update_materials.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Update materials called with $1 $2
+#$1 - temp dir
+#$2 - target dir
+rm -rf "$1"
+mkdir -p "$1"
+/var/www/materials/script/create_materials_tree.py "$1"
+rsync --partial -avrp --delete "$1"/ "$2"
+
