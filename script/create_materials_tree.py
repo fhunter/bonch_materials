d3bc4d752491ed9631f9d1c00a63d0b59b4c0bc1
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-08-22 19:04:56 +0400

    Made almost working sync script

--- script/create_materials_tree.py
+++ script/create_materials_tree.py
@@ -27,35 +27,38 @@ materials_basepath  = os.path.abspath(sys.argv[0])
 materials_basepath  = os.path.dirname(materials_basepath)
 materials_basepath  = os.path.abspath(materials_basepath + "/../materials")
 
-#Форма обучения -> специальность -> год -> дисциплина -> преподаватель -> материал
-#CREATE TABLE belongs (id integer primary key asc autoincrement, material_uuid text references materials (uuid) on update restrict on delete restrict, speciality_uuid text references speciality (uuid) on update restrict on delete restrict, student_year numeric not null default 1, study_form_uuid text references study_form (uuid) on update restrict on delete restrict, discipline_uuid text references discipline (uuid) on update restrict on delete restrict);
+#rsync -avrp ./materials/ /tmp/2/raw
+
+def mkdir(base,pathlist):
+	path = base + "/"
+	for i in pathlist:
+		path = path + "/" + i
+		try:
+			os.mkdir(path)
+		except:
+			pass
+def tostring(pathlist):
+	path = "."
+	for i in pathlist:
+		path = path + "/" + i
+	return path
+
+def link(path1,path2):
+	print "ln -s %s %s" % (path1, path2) 
+	try:
+		os.symlink(path1,path2)
+	except:
+		pass
 
 conn = sqlite3.connect("/var/www/materials/materials.sqlite")
 cursor = conn.cursor()
-cursor.execute("select study_form.study_form,study_form.uuid from study_form, belongs where belongs.study_form_uuid == study_form.uuid group by study_form.uuid")
-study_forms = cursor.fetchall()
-#Here goes addition of first directory level
-for (k,l) in study_forms:
-	cursor.execute("select speciality.code,speciality.name,speciality.uuid,belongs.student_year,belongs.material_uuid from belongs,speciality where belongs.speciality_uuid == speciality.uuid and belongs.study_form_uuid == ? group by belongs.student_year", (l,))
-	speciality = cursor.fetchall()
-	for (m,n,o,p,p1) in speciality:
-		cursor.execute("select discipline.name, discipline.semester from discipline,belongs where belongs.discipline_uuid == discipline.uuid")
-		discipline = cursor.fetchall()
-		for (q,r) in discipline:
-			cursor.execute("select authors.fio from authorship, authors where authors.uuid == authorship.author_uuid and authorship.material_uuid == ?", (p1,))
-			authors = cursor.fetchall()
-			for t in authors:
-				print u"%s/%s-%s/%s/%s семестр %s/%s/%s"%(k,m,n,p,q,r,t[0],p1)
+cursor.execute("select belongs.material_uuid,materials.name,study_form.study_form,speciality.code,speciality.name,belongs.student_year,discipline.name,discipline.semester from study_form, belongs, discipline, speciality,materials  where belongs.study_form_uuid == study_form.uuid and belongs.discipline_uuid == discipline.uuid and belongs.speciality_uuid == speciality.uuid and materials.uuid == belongs.material_uuid")
+results = cursor.fetchall()
+for i in results:
+	(material_uuid, materials_name, study_form, speciality_code, speciality_name, student_year, discipline, semester) = i
+	cursor.execute("select authors.fio from authors,authorship where authors.uuid == authorship.author_uuid and authorship.material_uuid == ?",(material_uuid,))
+	for j in cursor.fetchall():
+		mkdir(path, (study_form, speciality_code + "_" + speciality_name, u"Год обучения "+str(student_year), discipline, u"Семестр"+str(semester), j[0]))
+		link(path + "/raw/" + strip_uuid(material_uuid), path + "/" + tostring((study_form, speciality_code + "_" + speciality_name, u"Год обучения "+str(student_year), discipline, u"Семестр"+str(semester), j[0], materials_name)))
+		#Сделать здесь симлинк
 conn.close()
-
-#def makesetofdirs(basepath,dirset):
-#	for i in dirset:
-#		try:
-#			os.mkdir(basepath + "/" + i)
-#		except:
-#			pass
-#
-#makesetofdirs(path,["raw","by_author","by_speciality","by_year", "by_discipline", "by_studyform"])
-#os.system("rsync -arcp --delete %s %s" % (materials_basepath + "/", path + "/raw/"))
-#
-#makesetofdirs(path + "/by_studyform",list1)
