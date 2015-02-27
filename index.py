08d1d7e68bb1bdcbf92c87fd8fdae01ab83cb296
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2015-02-27 17:58:10 +0300

    Added initial template for study form edit/delete/add

--- index.py
+++ index.py
@@ -26,6 +26,10 @@ def authors():
 	result=get_authors()
 	return dict(data = result, headers = (u"ФИО автора",), width = (False,))
 
+@route('/authors/delete/<uuid>')
+def author_delete(uuid):
+	return ""
+
 @route('/discipline')
 @view('discipline')
 def discipline():
@@ -44,13 +48,31 @@ def study_form():
 	result=get_study_form()
 	return dict(data = result, headers = (u"Форма обучения",), width=(False,))
 
+@route('/study_form/delete/<uuid>')
+def study_form_delete(uuid):
+	return ""
+
+@route('/study_form/edit/<uuid>')
+def study_form_edit(uuid):
+	return ""
+
+@route('/study_form/edit/<uuid>',method='POST')
+def study_form_edit_post(uuid):
+	return ""
+
+@route('/study_form/add')
+def study_form_add():
+	return ""
+
+
+#This is access for our css file
 @route('/<filename:re:.*\.css>')
 def send_image(filename):
 	return static_file(filename, root='./files/', mimetype='text/css')
 
+#This is files for material access
 @route('/materials/<filename:re:.*>')
 def send_image(filename):
-	#
 	return static_file(filename, root='./')
 
 bottle.run(server=bottle.CGIServer) 
