33fc85f4e7e837e41126b3d78fe843a3e9ae8b96
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2015-03-14 20:22:30 +0300

    Updated scripts

--- script/update_materials.sh
+++ script/update_materials.sh
@@ -5,5 +5,5 @@ echo Update materials called with $1 $2
 rm -rf "$1"
 mkdir -p "$1"
 /var/www/materials/script/create_materials_tree.py "$1"
-rsync --partial -avrpL --delete "$1"/ "$2"
+rsync --partial -avrpl --delete "$1"/ "$2"
 chown -R root:materials "$2"
